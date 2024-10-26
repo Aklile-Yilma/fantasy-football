0d826a9c6792335f4f0709c7d5e510fcf5dab470
aklile-yilma <aklileyilma@gmail.com>
2024-10-26 15:07:07 +0300

    feat(navbar): add navbar
    this commit adds a navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import FantacyHome from "@/components/home/FantacyHome";
+import NavBar from "@/components/layout/NavBar";
 import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-background font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen overflow-y-scroll p-8 pb-20 gap-16 sm:p-20 bg-background font-[family-name:var(--font-geist-sans)]">
+      <NavBar />
       <FantacyHome />
       <Toaster />
     </div>
